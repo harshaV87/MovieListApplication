727bd71478945e75d9689998a9acfafc182506e9
“harshaV87” <harsha.87b@gmail.com>
2021-04-15 22:08:11 -0400

    Initial Commit

---
+++ MovieListApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieListApplication
+//
+//  Created by Venkata harsha Balla on 4/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MovieListApplicationTests/MovieListApplicationTests.swift
@@ -0,0 +1,33 @@
+//
+//  MovieListApplicationTests.swift
+//  MovieListApplicationTests
+//
+//  Created by Venkata harsha Balla on 4/15/21.
+//
+
+import XCTest
+@testable import MovieListApplication
+
+class MovieListApplicationTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
